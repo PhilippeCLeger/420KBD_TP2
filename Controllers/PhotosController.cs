5723cf16ed3610f03db6a1e85cfda86222bf8de8
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2022-04-22 13:20:22 -0400

    mise à jour du modèle de données

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsersManager.Controllers
+{
+    public class PhotosController : Controller
+    {
+        // GET: Photos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ SQL/CreateTables.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[PhotoVisibilities] (
+[Id] INT IDENTITY (1, 1) NOT NULL,
+[Name] NVARCHAR (50) NULL,
+PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Photos] (
+[Id] INT IDENTITY (1, 1) NOT NULL,
+[UserId] INT NOT NULL,
+[Title] NVARCHAR (50) NOT NULL,
+[Description] NVARCHAR (MAX) NULL,
+[VisibilityId] INT NOT NULL,
+[GUID] NVARCHAR (36) NOT NULL,
+[CreationDate] DATETIME NOT NULL,
+[Ratings] FLOAT (53) NOT NULL,
+[RatingsCount] INT NULL,
+PRIMARY KEY CLUSTERED ([Id] ASC),
+FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+FOREIGN KEY ([VisibilityId]) REFERENCES [dbo].[PhotoVisibilities] ([Id])
+);
+
+CREATE TABLE [dbo].[PhotoRatings] (
+[Id] INT IDENTITY (1, 1) NOT NULL,
+[PhotoId] INT NOT NULL,
+[UserId] INT NOT NULL,
+[Comment] NVARCHAR (MAX) NOT NULL,
+[Rating] INT NOT NULL,
+[CreationDate] DATETIME NOT NULL,
+PRIMARY KEY CLUSTERED ([Id] ASC),
+FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+FOREIGN KEY ([PhotoId]) REFERENCES [dbo].[Photos] ([Id])
+);
